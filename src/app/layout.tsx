eba74241227d4c672d3657d9106e74a266ce9b10
4t1lucifer-cmd <4t1lucifer@gmail.com>
2026-01-28 04:51:31 +0530

    refactor(ui): remove chatbot component as requested

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Chatbot from "@/components/Chatbot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Chatbot />
         {children}
       </body>
     </html>
